ed21cf4e0ca2d0e418b76324d986278404cd2ba5
ArjanRadja <arjanradja@gmail.com>
2025-02-08 16:07:16 -0300

    Add files via upload

---
+++ geboortedatum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;    
+
+int main() {
+  int jaartal =2025;
+  int leefttijd;
+
+ cout << "Wat is je leeftijd? ";   
+    cin >> leefttijd;   
+
+cout << "Je bent geboren in " << jaartal - leefttijd << endl;   
+    return 0;
+}
